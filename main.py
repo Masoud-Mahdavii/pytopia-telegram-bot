92c22c75c4700f9c9f4c49d489e114f3052333c9
Masoud Mahdavi <81777307+Masoud-Mahdavii@users.noreply.github.com>
2022-12-19 14:37:36 +0330

    Initialize master

---
+++ main.py
@@ -0,0 +1,18 @@
+n = []
+for i in n:
+    i = new_list[]
+    if i == 0:
+        print(i)
+        
+
+
+def add(a, b):
+    return a + b
+
+first_add=add(3, 4)
+first_add
+
+
+print("masoud mahdavi")
+print("mahda amini")
+print("hello qatar")
